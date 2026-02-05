8f2cd38d68a960496143d3a9d0de5fea30b13b29
Gayathri T P <tpgayathritp@gmail.com>
2026-02-05 12:07:52 +1000

    Update Payroll.py

--- Payroll.py
+++ Payroll.py
@@ -4,6 +4,8 @@ import logging
 from datetime import datetime
 from fpdf import FPDF
 from Database import get_connection, init_db
+from seed_employees import seed_employees
+
 
 # -----------------------------
 # Log files
